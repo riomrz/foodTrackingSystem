85926882bb06fda471b8887e38a72ce292f76045
MauriBTC <iorimaurizio95@gmail.com>
2022-12-30 18:35:03 +0100

    fixed find producet temporarily

--- foodTrackingSystem/views.py
+++ foodTrackingSystem/views.py
@@ -14,18 +14,19 @@ def insert_code(request):
         print('SONO NELLA POST')
         print('request.POST: ', request.POST)
         form = ProductForm(request.POST)
-        if form.is_valid():
-            print('form is valid')
-            code = request.POST.get('code')
-            print('code: ', code)
-            product = Product()
-            products = Product.objects.filter(code=code) # sempre singolo product
-            print(products)
-            if products:
-                product = products.get()
-                print('product: ', product)
-                return render(request, 'foodTrackingSystem/product_detail.html', {'form': form, 'product': product})
-            return render(request, 'foodTrackingSystem/product_detail.html', {'form': form})
+        # if form.is_valid(): --> PROBLEMA: FORSE AVER MESSO LA CHIAVE UNIQUE A CODE, IL FORM PER LA RICERCA VIENE FRAINTESO COME SE FOSSE PER L'INSERIMENTO NUOVO PRODOTTO
+        print('form is valid')
+        code = request.POST.get('code')
+        print('code: ', code)
+        product = Product()
+        products = Product.objects.filter(code=code) # sempre singolo product
+        print(products)
+        if products:
+            product = products.get()
+            print('product: ', product)
+            return render(request, 'foodTrackingSystem/product_detail.html', {'form': form, 'product': product})
+        return render(request, 'foodTrackingSystem/product_detail.html', {'form': form})
+        
         
         
 
